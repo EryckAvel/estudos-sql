67b053e28e8cde31c8c6922e7ef882b2d329ae9a
eryckavel <eryckavel@gmail.com>
2023-04-26 15:15:07 -0300

    alteração no script de funções

--- SQL com PostgreSQL/01/PostgreSQLViews,SubConsultaseFunções/usandofuncoes.sql
+++ SQL com PostgreSQL/01/PostgreSQLViews,SubConsultaseFunções/usandofuncoes.sql
@@ -2,5 +2,82 @@
 
 -- FUNÇÕES DE STRING 
 
-SELECT (nome junto com sobrenome) AS nome_completo FROM aluno;
+-- JUNTAR STRINGS 
+
+SELECT (nome || ' ' || sobrenome) AS nome_completo FROM aluno;
+
+SELECT CONCAT('Eryck', ' ', NULL)
+
+-- EXISTES OUTRAS FUNÇÕES PARA STRINGS QUE PODEM SER PESQUISADAS
+
+-- EX:
+
+SELECT UPPER('eryck'); -- COLOCA TUDO EM CAIXA ALTA
+SELECT LOWER('NICOLAS'); -- COLOCA TUDO EM CAIXA BAIXA
+
+-- FUNÇÕES DE DATA --
+
+SELECT
+	(nome || ' ' || sobrenome) AS nome_completo,
+	datanascimento as data_nascimento,
+	NOW()::DATE AS data_atual,
+	(NOW()::DATE - datanascimento)/365 AS idade,
+	AGE(datanascimento) as idade_nova,
+	EXTRACT(YEAR FROM AGE(datanascimento)) as idade_ano
+FROM aluno;
+
+-- FUNCÕES MATEMATICAS -- 
+
+SELECT 
+	10+20 AS soma,
+	10-20 AS subtracao,
+	10/20 AS divisao,
+	10*20 AS multiplicacao,
+	FACTORIAL(5) AS fatorial,
+	|/25 AS raiz_quadrada,
+	||/25 AS raiz_cubica,
+	2 ^ 3 AS exponencial,
+	5 % 4 AS modulo;
+
+SELECT 
+	PI() AS valor_de_pi,
+	@ -12345675678 AS valor_absoluto,
+	DIV(10/20) AS divisao,
+	
+
+-- CONVERSÕES DE DADOS --
+
+SELECT TO_CHAR(NOW(), 'DD/MONTH/YYYY');
+
+SELECT TO_CHAR(128::REAL, '999D99');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
